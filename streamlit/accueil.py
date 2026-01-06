e56e5f1b40bc67b34a6013ec588da5c8e61f8727
barnabew <barnabe.willenbucher@outlook.fr>
2026-01-06 11:31:04 +0100

    nouvelle accueil

--- streamlit/accueil.py
+++ streamlit/accueil.py
@@ -26,13 +26,13 @@ with col1:
     st.markdown(render_kpi_card("Différences CA promo", "1.92%"),unsafe_allow_html=True)
     
 with col2:
-    st.markdown(render_kpi_card("TOP département", "95"),unsafe_allow_html=True)
+    st.markdown(render_kpi_card("Nombre départements universels (présence 100%)", "62/81"),unsafe_allow_html=True)
     
 with col3:
     st.markdown(render_kpi_card("TOP 5 Départements communs A et C", "92,95,90,38"),unsafe_allow_html=True)
     
 with col4:
-    st.markdown(render_kpi_card("Différence CA décembre type A", "10%"),unsafe_allow_html=True)
+    st.markdown(render_kpi_card("Variation CA décembre type A et B", "23%"),unsafe_allow_html=True)
 
 
 
@@ -50,6 +50,16 @@ with chart_row1[1]:
     # Évolution Temporelle des Types de Magasins
     st.plotly_chart(visuel.plot_evolution_temporelle_types(run_query(queries.QUERY_EVOL_TEMP_TYPE)), use_container_width=True)
 
+chart_row2 = st.columns(2, gap="large")
+
+df_dept_stars = run_query(queries.QUERY_DEPT_STARS)
+def_dept_segmentation = run_query(queries.QUERY_CATEGORIE_DEPTS)
+
+with chart_row2[0]:
+   st.markdown(styles.render_table_window("Détail des ventes", df_dept_stars[df_dept_stars["Rang"]<=5]),unsafe_allow_html=True)
+
+with chart_row2[1]:
+   st.markdown(styles.render_table_window("Segmentation des départements", def_dept_segmentation),unsafe_allow_html=True)
 
 
 st.title("Recommandations")
